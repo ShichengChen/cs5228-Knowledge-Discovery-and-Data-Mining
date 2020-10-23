a88db126585a8c2bcee2f65dd621c99c08d75301
Shicheng Chen <c337134154@gmail.com>
2020-10-24 01:21:41 +0800

    feature engineering on date, change years to date, add missed date, add missed state

--- finalProject/featureEngineer.py
+++ finalProject/featureEngineer.py
@@ -1,8 +1,3 @@
-# coding: utf-8
-
-# In[1]:
-
-
 # data manipulation
 import pandas as pd
 import numpy as np
@@ -10,9 +5,6 @@ import math
 # visualization
 import seaborn as sb
 import matplotlib.pyplot as plt
-
-#get_ipython().run_line_magic('matplotlib', 'inline')
-
 # model training
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
@@ -21,78 +13,89 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
-
+from datetime import datetime
 # classifiers
-from sklearn.naive_bayes import GaussianNB  # naive bayes
-from sklearn.neighbors import KNeighborsClassifier  # KNN
-from sklearn.linear_model import LogisticRegression  # logistic regression
-from sklearn.tree import DecisionTreeClassifier  # decision Tree
+from sklearn.naive_bayes import GaussianNB # naive bayes
+from sklearn.neighbors import KNeighborsClassifier # KNN
+from sklearn.linear_model import LogisticRegression # logistic regression
+from sklearn.tree import DecisionTreeClassifier # decision Tree
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from xgboost import XGBClassifier
-from sklearn.model_selection import StratifiedKFold, RandomizedSearchCV
+from sklearn.model_selection import StratifiedKFold,RandomizedSearchCV
 import lightgbm as lgb
 from datetime import datetime
 # ignore warnings
 import warnings
 from sklearn.ensemble import VotingClassifier
-
+warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
+from dateutil.parser import parse
+
 
 # In[3]:
-
+from uszipcode import SearchEngine
 
 showimage = False
 
 
 def fe(df):
-    df["State"][(df["State"].isna())] = 'nan'
-    unique, pos = np.unique(df['State'], return_inverse=True)
-    counts = np.bincount(pos)
-    maxpos = counts.argmax()
-    df['State'][df['State'] == 'nan'] = unique[maxpos]
+
+    search = SearchEngine(simple_zipcode=True)
+    state = df["Zip"][(df["State"].isna())].apply(lambda x: search.by_zipcode(x).state)
+    print(state)
+    df["State"][(df["State"].isna())] = state
 
     df["LowDoc"][(df["LowDoc"].notna()) & (df["LowDoc"] != 'Y')] = 'N'
     df["LowDoc"][df["LowDoc"].isna()] = 'nan'
     df["RevLineCr"][(df["RevLineCr"].notna()) & (df["RevLineCr"] != 'Y')] = 'N'
     df["RevLineCr"][df["RevLineCr"].isna()] = 'nan'
 
-
-    df["DisbursementDate"][df["DisbursementDate"].isna()] = '19-Oct-20'
-
+    print(np.sum(df["DisbursementDate"].isna()),np.sum(df["ApprovalDate"].isna()),)
+    import datetime
+    def parsedate(x):
+        dt = parse(x)
+        d = (dt - parse('19-Oct-20')).days
+        if (d >= 0):
+            dt = datetime.datetime(dt.year - 100, dt.month, dt.day)
+        return dt
     # In[11]:
 
+    DisbursementDate = \
+        df['DisbursementDate'][df["DisbursementDate"].notna()]. \
+            apply(lambda x: parsedate(x))
 
-    df['DisbursementDate'] = df["DisbursementDate"].apply(str)
-    df['DisbursementDate'] = (df['DisbursementDate'].str.split("-").str[-1])
-    assert(np.sum(df["DisbursementDate"].isna())==0)
-    df['DisbursementDate'] = df['DisbursementDate'].astype(float)
-    df['DisbursementDate'] = df['DisbursementDate'].astype(int)
+    ApprovalDate = \
+        df['ApprovalDate'][df["DisbursementDate"].notna()]. \
+            apply(lambda x: parsedate(x))
+    difdays = (DisbursementDate - ApprovalDate).apply(lambda x: x.days)
+    mdifdays = difdays.median()
+    print('mdifdays', mdifdays)
 
-    # In[13]:
+    disDatenan = \
+        df['ApprovalDate'][df["DisbursementDate"].isna()]. \
+            apply(lambda x: parse(x) + datetime.timedelta(days=mdifdays))
+    df['DisDate'] = parse('19-Oct-21')
+    df['DisDate'][df["DisbursementDate"].isna()] = disDatenan
+    df['DisDate'][df["DisbursementDate"].notna()] = DisbursementDate
+    assert np.sum((df['DisDate'] - parse('19-Oct-20')).apply(lambda x: x.days) >= 0) == 0
+    df['ApprovalDate'] = df['ApprovalDate'].apply(lambda x: parsedate(x))
+    df['before'] = ((parse("01-Dec-2007") > df['DisDate'])).astype(int)
+    df['recession'] = ((parse("01-Dec-2007") <= df['DisDate']) & \
+                       (df['DisDate'] <= parse("30-Jun-2009"))).astype(int)
+    # df['nowadays']=((parse("01-Sep-2010")<=df['DisDate'])).astype(int)
+    df['nowadays'] = ((parse("30-Jun-2009") <= df['DisDate'])).astype(int)
+    basedate = parse("20-Sep-1968")
 
+    df['Appday'] = (df['ApprovalDate'] - basedate).apply(lambda x: x.days)
 
-    df['DisbursementDate'] = df['DisbursementDate'].apply(lambda x: x + 2000 if x < 25 else x + 1900)
+    df['Disday'] = (df['DisDate'] - basedate).apply(lambda x: x.days)
 
-    # In[14]:
+    df.drop(['DisbursementDate', 'DisDate', 'ApprovalDate', 'ApprovalFY'], axis=1, inplace=True)
 
-
-    if showimage:
-        sb.countplot(x='DisbursementDate', data=df)
-        plt.show()
-
-    # In[15]:
-
-
-    df['ApprovalFY'] = df['ApprovalFY'].str.replace(r'\D', '')
-    df["ApprovalFY"][df["ApprovalFY"].isna()] = df["ApprovalFY"][df["ApprovalFY"].notna()].median()
-
-    df['ApprovalFY'] = df['ApprovalFY'].astype(int)
-
-    # In[16]:
 
 
     dollor = ['DisbursementGross', 'GrAppv', 'SBA_Appv']
@@ -195,8 +198,6 @@ def fe(df):
     df['SequBS'] = (df['State'] == df['BankState']).astype(int)
     df['portion'] = df['SBA_Appv'] / df['GrAppv']
     df['realstate'] = (df['Term'] > 240).astype(int)
-    df['recession'] = ((2007 <= df['ApprovalFY']) & (df['ApprovalFY'] <= 2009)).astype(int)
-    df['nowadays'] = ((2010 <= df['ApprovalFY'])).astype(int)
 
 
     nai = {11: 'Agriculture', 21: 'Mining', 22: 'Utilities', 23: 'Construction', 31: 'Manufacturing',
@@ -218,8 +219,7 @@ def fe(df):
     #     print(i, df[i].nunique())
 
 
-    df = df.drop(['Bank','Name','City','Zip','ApprovalDate','BalanceGross','Id'], 1)
-
+    df = df.drop(['Bank', 'Name', 'City', 'Zip', 'BalanceGross', 'Id'], 1)
     # In[44]:
 
 
@@ -227,15 +227,16 @@ def fe(df):
         sb.clustermap(df.corr(), annot=True)
         plt.show()
 
-    tostr = ['FranchiseCode', 'NewExist', 'UrbanRural', 'State', 'BankState', 'RevLineCr',
-             'LowDoc', 'NAICS2']
-    extra = []
+    tostr = ['FranchiseCode', 'NewExist', 'UrbanRural', 'RevLineCr',
+             'LowDoc', 'NAICS2',
+             'State', 'BankState',
+             ]
+    # extra = []
+    # df = df.drop(extra, 1)
+
     noneedforonehot = ['nowadays', 'recession', 'realstate', 'SequBS']
     for i in tostr:
         df[i] = df[i].astype(str)
-    for i in tostr:
-        df[i] = df[i].astype(str)
-
 
     df = pd.get_dummies(df)
 
@@ -269,7 +270,7 @@ if __name__ == '__main__':
 
     # In[52]:
 
-
+ 
     def timer(start_time=None):
         if not start_time:
             start_time = datetime.now()
@@ -286,13 +287,13 @@ if __name__ == '__main__':
         ('2',cb.CatBoostClassifier(random_seed=0,silent=True,
                      **{'learning_rate': 0.1, 'l2_leaf_reg': 6, 'iterations': 900, 'depth': 6})),
         ('3',lgb.LGBMClassifier(random_state=0,silent = True,
-                     **{'subsample': 0.8, 'num_leaves': 100, 'n_estimators': 1500, 'min_split_gain': 0.3, 'max_depth': 20, 'colsample_bytree': 0.7})),
+                     **{'subsample': 0.8, 'num_leaves': 100, 'n_estimators': 1500, 'min_split_gain': 0.5, 'max_depth': 20, 'colsample_bytree': 0.7})),
         ('4', XGBClassifier(objective='binary:logistic', silent=True, nthread=2, seed=1, verbosity=0,
-                     **{'subsample': 0.9, 'n_estimators': 700, 'min_child_weight': 1, 'max_depth': 10, 'learning_rate': 0.05, 'colsample_bytree': 0.7})),
+                     **{'subsample': 1, 'n_estimators': 700, 'min_child_weight': 2, 'max_depth': 10, 'learning_rate': 0.03, 'colsample_bytree': 0.9})),
         ('5', cb.CatBoostClassifier(random_seed=1, silent=True,
-                     **{'learning_rate': 0.1, 'l2_leaf_reg': 6, 'iterations': 900, 'depth': 8})),
+                     **{'learning_rate': 0.1, 'l2_leaf_reg': 9, 'iterations': 700, 'depth': 7})),
         ('6', lgb.LGBMClassifier(random_state=1, silent=True,
-                     **{'subsample': 0.7, 'num_leaves': 100, 'n_estimators': 700, 'min_split_gain': 0.5, 'max_depth': 20, 'colsample_bytree': 0.7})),
+                     **{'subsample': 0.8, 'num_leaves': 40, 'n_estimators': 1000, 'min_split_gain': 0.5, 'max_depth': 25, 'colsample_bytree': 0.7})),
     ]
 
     # In[ ]:
@@ -312,158 +313,4 @@ if __name__ == '__main__':
     y_pred=y_pred.tolist()
     ans=pd.DataFrame(list(zip(id, y_pred)), columns=['Id', 'ChargeOff'])
     ans.to_csv("ans.csv",index=False)
-
-    #  Time taken: 0 hours 36 minutes and 32.98 seconds.
-    # {'subsample': 0.9, 'n_estimators': 700, 'min_child_weight': 3, 'max_depth': 10, 'learning_rate': 0.05, 'colsample_bytree': 0.7}
-    # 93.05
-
-    #  Time taken: 0 hours 49 minutes and 33.57 seconds.
-    # {'learning_rate': 0.15, 'l2_leaf_reg': 9, 'iterations': 300, 'depth': 7}
-    # 92.82
-
-    #  Time taken: 0 hours 35 minutes and 11.3 seconds.
-    # {'subsample_freq': 20, 'subsample': 0.8, 'num_leaves': 50, 'n_estimators': 700, 'min_split_gain': 0.4, 'max_depth': 15, 'colsample_bytree': 0.8}
-    # 92.89
-    # 93.2
-'''
-######################
-'''
-
-    # {'subsample': 0.9, 'n_estimators': 700, 'min_child_weight': 1, 'max_depth': 10, 'learning_rate': 0.05,
-    # 'colsample_bytree': 0.7}
-    #  93.27
-    #
-    # {'learning_rate': 0.15, 'l2_leaf_reg': 9, 'iterations': 500, 'depth': 7}
-    # 93.16
-    #
-    # {'subsample_freq': 20, 'subsample': 0.9, 'num_leaves': 50, 'n_estimators': 1000, 'min_split_gain': 0.3, 'max_depth': 20,
-    #  'colsample_bytree': 0.7}
-    # 93.06
-    #
-    # {'subsample': 0.8, 'n_estimators': 400, 'min_child_weight': 1, 'max_depth': 20, 'learning_rate': 0.05,
-    #  'colsample_bytree': 0.9}
-    # 92.82
-    #
-    # {'learning_rate': 0.15, 'l2_leaf_reg': 9, 'iterations': 500, 'depth': 7}
-    # 93.28
-    #
-    # {'subsample_freq': 20, 'subsample': 0.8, 'num_leaves': 50, 'n_estimators': 400, 'min_split_gain': 0.4, 'max_depth': 25,
-    #  'colsample_bytree': 0.8}
-    # 92.94
-    # 93.34
-    # public leadboard 0.93600
-
-#  Time taken: 0 hours 39 minutes and 43.73 seconds.
-# {'subsample': 0.8, 'n_estimators': 700, 'min_child_weight': 2, 'max_depth': 10, 'learning_rate': 0.03, 'colsample_bytree': 0.9}
-# 93.18
-#
-#  Time taken: 0 hours 30 minutes and 3.27 seconds.
-# {'learning_rate': 0.1, 'l2_leaf_reg': 6, 'iterations': 900, 'depth': 6}
-# 93.29
-#
-#  Time taken: 0 hours 10 minutes and 31.77 seconds.
-# {'subsample': 0.8, 'num_leaves': 40, 'n_estimators': 700, 'min_split_gain': 0.5, 'max_depth': 25, 'colsample_bytree': 0.7}
-# 93.13
-#
-#  Time taken: 0 hours 42 minutes and 37.97 seconds.
-# {'subsample': 0.9, 'n_estimators': 700, 'min_child_weight': 1, 'max_depth': 10, 'learning_rate': 0.05, 'colsample_bytree': 0.7}
-# 93.34
-#
-#  Time taken: 0 hours 28 minutes and 46.9 seconds.
-# {'learning_rate': 0.15, 'l2_leaf_reg': 13, 'iterations': 700, 'depth': 7}
-# 93.18
-#
-#  Time taken: 0 hours 10 minutes and 31.51 seconds.
-# {'subsample': 0.8, 'num_leaves': 40, 'n_estimators': 1000, 'min_split_gain': 0.5, 'max_depth': 25, 'colsample_bytree': 0.7}
-# 93.06
-# 93.4
-# public leadboard 0.93582
-
-
-'''
-#################################
-'''
-
-#  Time taken: 0 hours 44 minutes and 34.87 seconds.
-# {'subsample': 0.8, 'n_estimators': 700, 'min_child_weight': 2, 'max_depth': 10, 'learning_rate': 0.03, 'colsample_bytree': 0.9}
-# 93.14
-#
-#  Time taken: 0 hours 30 minutes and 46.12 seconds.
-# {'learning_rate': 0.1, 'l2_leaf_reg': 6, 'iterations': 900, 'depth': 6}
-# 93.21
-#
-#  Time taken: 0 hours 12 minutes and 17.61 seconds.
-# {'subsample': 0.8, 'num_leaves': 100, 'n_estimators': 1500, 'min_split_gain': 0.5, 'max_depth': 20, 'colsample_bytree': 0.7}
-# 93.14
-#
-#  Time taken: 0 hours 47 minutes and 56.8 seconds.
-# {'subsample': 0.9, 'n_estimators': 700, 'min_child_weight': 1, 'max_depth': 10, 'learning_rate': 0.05, 'colsample_bytree': 0.7}
-# 93.22
-#
-#  Time taken: 0 hours 29 minutes and 40.96 seconds.
-# {'learning_rate': 0.1, 'l2_leaf_reg': 9, 'iterations': 700, 'depth': 7}
-# 93.13
-#
-#  Time taken: 0 hours 9 minutes and 34.31 seconds.
-# {'subsample': 0.8, 'num_leaves': 40, 'n_estimators': 1000, 'min_split_gain': 0.5, 'max_depth': 25, 'colsample_bytree': 0.7}
-# 93.07
-# 93.42
-
-'''
-#####################################################################################
-'''
-
-
-#  Time taken: 2 hours 11 minutes and 20.16 seconds.
-# {'subsample': 0.8, 'n_estimators': 700, 'min_child_weight': 2, 'max_depth': 10, 'learning_rate': 0.03, 'colsample_bytree': 0.9}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 0 hours 9 minutes and 4.07 seconds.
-# {'learning_rate': 0.1, 'l2_leaf_reg': 9, 'iterations': 700, 'depth': 8}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 0 hours 1 minutes and 20.6 seconds.
-# {'subsample': 0.8, 'num_leaves': 100, 'n_estimators': 1500, 'min_split_gain': 0.5, 'max_depth': 20, 'colsample_bytree': 0.7}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 2 hours 23 minutes and 57.58 seconds.
-# {'subsample': 0.9, 'n_estimators': 700, 'min_child_weight': 1, 'max_depth': 10, 'learning_rate': 0.05, 'colsample_bytree': 0.7}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 0 hours 11 minutes and 11.29 seconds.
-# {'learning_rate': 0.15, 'l2_leaf_reg': 13, 'iterations': 700, 'depth': 7}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 0 hours 1 minutes and 16.81 seconds.
-# {'subsample': 0.7, 'num_leaves': 50, 'n_estimators': 700, 'min_split_gain': 0.4, 'max_depth': 15, 'colsample_bytree': 0.7}
-# make_scorer(roc_auc_score, needs_threshold=True)
-# full trainning dataset
-# 0.93612
-
-'''
-#############
-'''
-#  Time taken: 2 hours 7 minutes and 48.43 seconds.
-# {'subsample': 0.8, 'n_estimators': 700, 'min_child_weight': 2, 'max_depth': 10, 'learning_rate': 0.03, 'colsample_bytree': 0.9}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 0 hours 9 minutes and 3.15 seconds.
-# {'learning_rate': 0.1, 'l2_leaf_reg': 11, 'iterations': 900, 'depth': 6}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 0 hours 1 minutes and 19.43 seconds.
-# {'subsample': 0.8, 'num_leaves': 100, 'n_estimators': 1500, 'min_split_gain': 0.3, 'max_depth': 20, 'colsample_bytree': 0.7}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 2 hours 20 minutes and 57.15 seconds.
-# {'subsample': 0.9, 'n_estimators': 700, 'min_child_weight': 1, 'max_depth': 10, 'learning_rate': 0.05, 'colsample_bytree': 0.7}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 0 hours 10 minutes and 58.81 seconds.
-# {'learning_rate': 0.1, 'l2_leaf_reg': 6, 'iterations': 900, 'depth': 8}
-# make_scorer(roc_auc_score, needs_threshold=True)
-#
-#  Time taken: 0 hours 1 minutes and 15.86 seconds.
-# {'subsample': 0.7, 'num_leaves': 100, 'n_estimators': 700, 'min_split_gain': 0.5, 'max_depth': 20, 'colsample_bytree': 0.7}
-# make_scorer(roc_auc_score, needs_threshold=True)
 
